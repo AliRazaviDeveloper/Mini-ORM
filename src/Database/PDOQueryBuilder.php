b7c375545598acda8e40c84174d7c6943a0c9647
AliRazaviDeveloper <alirazavi.developer@gmail.com>
2023-09-02 17:44:56 +0330

    feat: teardown after test run trancate database

--- src/Database/PDOQueryBuilder.php
+++ src/Database/PDOQueryBuilder.php
@@ -7,17 +7,15 @@ use App\Contract\DatabaseInterface;
 class PDOQueryBuilder
 {
     protected \PDO $connection;
-    protected $table;
-
+    protected string $table;
     protected array $condition;
-
     protected array $value;
     public function __construct(DatabaseInterface $connection)
     {
         $this->connection=$connection->getConnection();
     }
 
-    public function table($table)
+    public function table($table): static
     {
          $this->table=$table;
          return $this;
@@ -38,7 +36,7 @@ class PDOQueryBuilder
         return (int)$this->connection->lastInsertId();
     }
 
-    public function where(string $column,string $value)
+    public function where(string $column,string $value): static
     {
         $this->condition[]="{$column}=?";
         $this->value[]=$value;
@@ -57,5 +55,14 @@ class PDOQueryBuilder
         $query=$this->connection->prepare($sql);
         $query->execute($this->value);
         return (int)$query->rowCount();
+    }
+
+    public function trancateAll()
+    {
+        $query=$this->connection->prepare("show tables");
+        $query->execute();
+        foreach ($query->fetchAll(\PDO::FETCH_COLUMN) as $table ){
+            $this->connection->prepare("TRUNCATE TABLE `{$table}`")->execute();
+        }
     }
 }
